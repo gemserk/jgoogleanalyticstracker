c712b9c29aa5192926c2c789f13d884080885695
Ruben Garat <ruben.garat@gemserk.com>
2011-05-24 01:07:29 -0300

    extracted auto configuration of analytics info for the desktop

--- src/main/java/com/dmurph/tracking/AnalyticsConfigData.java
+++ src/main/java/com/dmurph/tracking/AnalyticsConfigData.java
@@ -25,10 +25,6 @@
  */
 package com.dmurph.tracking;
 
-import java.awt.DisplayMode;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.HeadlessException;
 
 /**
  * Data that is client-specific, and should be common for all tracking requests.
@@ -58,55 +54,9 @@ public class AnalyticsConfigData {
             throw new RuntimeException("Tracking code cannot be null");
         }
         trackingCode = argTrackingCode;
-        populateFromSystem();
     }
 
-    /**
-     * Populates user language, color depth, screen resolution, and character
-     * encoding. Can't get flash version.
-     */
-    public void populateFromSystem() {
-        encoding = System.getProperty("file.encoding");
-
-        String region = System.getProperty("user.region");
-        if (region == null) {
-            region = System.getProperty("user.country");
-        }
-        userLanguage = System.getProperty("user.language") + "-" + region;
-
-        int screenHeight = 0;
-        int screenWidth = 0;
-        
-        GraphicsEnvironment ge = null;
-        GraphicsDevice[] gs = null;
-        
-        try {
-            ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            gs = ge.getScreenDevices();
-    
-            // Get size of each screen
-            for (int i = 0; i < gs.length; i++) {
-                DisplayMode dm = gs[i].getDisplayMode();
-                screenWidth += dm.getWidth();
-                screenHeight += dm.getHeight();
-            }
-            if (screenHeight != 0 && screenWidth != 0) {
-                screenResolution = screenWidth + "x" + screenHeight;
-            }
-    
-            if (gs[0] != null) {
-                colorDepth = gs[0].getDisplayMode().getBitDepth() + "";
-                for (int i = 1; i < gs.length; i++) {
-                    colorDepth += ", " + gs[i].getDisplayMode().getBitDepth();
-                }
-            }
-        } catch (HeadlessException e) {
-            // report reasonable defaults.
-            screenHeight = 1024;
-            screenWidth = 768;
-            colorDepth = "32";
-        }
-    }
+ 
 
     /**
      * @return the colorDepth
